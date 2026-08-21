438373e5ee9729a026329c87c5a6efecfe66e1f5
Gabriel Villenet <147531932+VillenetMK@users.noreply.github.com>
2026-08-20 21:12:13 -0500

    Redesign EcoSphere color system for presentation UI

--- app/src/main/java/com/example/ecosphere/ui/theme/Color.kt
+++ app/src/main/java/com/example/ecosphere/ui/theme/Color.kt
@@ -2,10 +2,28 @@ package com.example.ecosphere.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val EcoGreen = Color(0xFF1F6B45)
+val EcoGreenDark = Color(0xFF8FD5AE)
+val EcoGreenContainer = Color(0xFFD8F3E3)
+val EcoGreenContainerDark = Color(0xFF164B32)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val EcoTeal = Color(0xFF386A63)
+val EcoTealDark = Color(0xFFA1D0C6)
+val EcoTealContainer = Color(0xFFBCECE2)
+val EcoTealContainerDark = Color(0xFF204E48)
+
+val EcoEarth = Color(0xFF6B5D3B)
+val EcoEarthDark = Color(0xFFD7C79A)
+
+val EcoBackground = Color(0xFFF6F8F5)
+val EcoSurface = Color(0xFFFCFDF9)
+val EcoSurfaceVariant = Color(0xFFE6E9E3)
+val EcoOnSurface = Color(0xFF1A1C1A)
+
+val EcoBackgroundDark = Color(0xFF111412)
+val EcoSurfaceDark = Color(0xFF191C1A)
+val EcoSurfaceVariantDark = Color(0xFF404943)
+val EcoOnSurfaceDark = Color(0xFFE1E4DF)
+
+val EcoError = Color(0xFFBA1A1A)
+val EcoErrorContainer = Color(0xFFFFDAD6)
