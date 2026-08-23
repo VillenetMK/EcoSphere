e5f609553203f1d852fec8772f2e4d3ca8b28ebe
Gabriel Villenet <147531932+VillenetMK@users.noreply.github.com>
2026-08-23 17:49:55 -0500

    fix: align shared core JVM targets

--- sharedCore/build.gradle.kts
+++ sharedCore/build.gradle.kts
@@ -14,6 +14,11 @@ kotlin {
     }
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
 dependencies {
     api(libs.gson)
     testImplementation(libs.junit)
