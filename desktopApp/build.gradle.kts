2ceb5ee6ed4b8cc3b3ec063a66a53613b757bdbc
Gabriel Villenet <147531932+VillenetMK@users.noreply.github.com>
2026-08-24 13:06:31 -0500

    chore(desktop): align installer version 1.4.2

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.example.ecosphere"
-version = "1.4.1"
+version = "1.4.2"
 
 kotlin {
     jvmToolchain(17)
@@ -39,7 +39,7 @@ compose.desktop {
                 org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb
             )
             packageName = "EcoSphere"
-            packageVersion = "1.4.1"
+            packageVersion = "1.4.2"
             description = "EcoSphere - Sistema inteligente de microclima"
             vendor = "EcoSphere"
             modules("java.net.http")
