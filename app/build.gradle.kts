6649e5f72c5b32609e4fbf96b3384b44d2ce487c
Gabriel Villenet <147531932+VillenetMK@users.noreply.github.com>
2026-08-24 13:51:47 -0500

    chore(android): bump version to 1.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "com.example.ecosphere"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.4.3"
+        versionCode = 9
+        versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
